636dfd5b821d1a53b197ad9061225e87fcaa66d6
Pascal Führlich <pascal.fuehrlich@web.de>
2017-03-14 23:56:45 +0100

    add image boxes and measures dynamically, bug fix

--- index.js
+++ index.js
@@ -47,15 +47,111 @@ $('#bottomDiv')
 		$(this).children().trigger('mlayout')
 	})
 
-const g = (function(){
+const blobs = {}
+
+function boxDragEnter(e) {stopProp(e)
+	$(this)
+		.css('box-shadow', '0 0 0 20px green')
+		.css('z-index', '1')
+}
+
+function boxDragLeave(e) {stopProp(e)
+	$(this)
+		.css('box-shadow', 'initial')
+		.css('z-index', 'initial')
+}
+
+function boxDropFunction(mcol, mrow) {
+	//saving col as mcol, otherwise col == g.columns() onDrop for each box
+	return function(e) {stopProp(e)
+		if (e.originalEvent.dataTransfer.files.length > 0) {
+			let mURL = window.URL.createObjectURL(e.originalEvent.dataTransfer.files[0])
+			blobs[[mcol, mrow]] = mURL
+			$(this).find('img')
+				.attr('src', mURL)
+				.trigger('load')
+		}
+		$(this).trigger('dragleave')
+	}
+}
+
+function scaleImage() {
+	let widthScale = $(this).parent().width() / $(this).prop('naturalWidth')
+	let heightScale = $(this).parent().height() / $(this).prop('naturalHeight')
+
+	if (widthScale > heightScale) {
+		$(this)
+		.width(widthScale * $(this).prop('naturalWidth'))
+		.height(widthScale * $(this).prop('naturalHeight'))
+		.css('top', -0.5 * ($(this).height() - $(this).parent().height()))
+	} else {
+		$(this)
+		.width(heightScale * $(this).prop('naturalWidth'))
+		.height(heightScale * $(this).prop('naturalHeight'))
+		.css('left', -0.5 * ($(this).width() - $(this).parent().width()))
+	}
+}
+
+function setHorizontalLines(lines, mwidth) {
+	lines[0].setAttribute('x1', 5)
+	lines[0].setAttribute('x2', mwidth - 5)
+	lines[0].setAttribute('y1', '50%')
+	lines[0].setAttribute('y2', '50%')
+
+	lines[1].setAttribute('x1', 5)
+	lines[1].setAttribute('x2', 5)
+	lines[1].setAttribute('y1', '0%')
+	lines[1].setAttribute('y2', '100%')
+
+	lines[2].setAttribute('x1', mwidth - 5)
+	lines[2].setAttribute('x2', mwidth - 5)
+	lines[2].setAttribute('y1', '0%')
+	lines[2].setAttribute('y2', '100%')
+}
+function setVerticalLines(lines, mheight) {
+	lines[0].setAttribute('y1', 5)
+	lines[0].setAttribute('y2', mheight - 5)
+	lines[0].setAttribute('x1', '50%')
+	lines[0].setAttribute('x2', '50%')
+
+	lines[1].setAttribute('y1', 5)
+	lines[1].setAttribute('y2', 5)
+	lines[1].setAttribute('x1', '0%')
+	lines[1].setAttribute('x2', '100%')
+
+	lines[2].setAttribute('y1', mheight - 5)
+	lines[2].setAttribute('y2', mheight - 5)
+	lines[2].setAttribute('x1', '0%')
+	lines[2].setAttribute('x2', '100%')
+}
+
+
+let g
+
+function boxLayoutFunction(mcol, mrow){
+	return function(e){stopProp(e)
+		if(mcol >= g.columns() || mrow >= g.rows()){
+			$(this).remove()
+		}else{
+			$(this)
+				.width(g.imageWidth() + 'mm')
+				.height(g.imageHeight() + 'mm')
+				.css('top', mrow * (g.imageMargin() + g.imageHeight()) + 'mm')
+				.css('left', mcol * (g.imageMargin() + g.imageWidth()) + 'mm')
+				.find('img').trigger('load')
+		}
+	}
+}
+
+g = (function(){
 	let _paperWidth = 210 //mm
 	let _paperHeight = 297 //mm
 
 	let _printerMargin = 10 //mm
 	let _imageMargin = 5 //mm
 
-	let _columns = 3 //mm
-	let _rows = 4 //mm
+	let _columns = 4
+	let _rows = 3
 
 	let _printWidth = 0 //mm
 	let _printHeight = 0 //mm
@@ -113,15 +209,103 @@ const g = (function(){
 			return _columns
 		},
 		setColumns: function(v){
+			//add new imageboxes and measures
+			console.log('setcols');
+			for (let col = _columns; col < v; col++) {
+				for (let row = 0; row < _rows; row++) {
+					//new image box
+					let x = $('<div class="box"><img></div>')
+					x
+						.appendTo($('#printArea'))
+						.on('mlayout', boxLayoutFunction(col, row))
+						.on('dragenter', boxDragEnter)
+						.on('dragover', stopProp)
+						.on('dragleave', boxDragLeave)
+						.on('drop', boxDropFunction(col, row))
+						.find('img')
+							.attr('src', blobs[[col, row]] || placeholder)
+							.on('load', scaleImage)
+				}
+
+				//new margin measure
+				if(col > 0){
+					addMeasure('#topDiv', (function(mcol){
+						return function(){
+							if(mcol >= g.imageMargin()){
+								$(this).remove()
+							}else{
+								$(this)
+									.css('left', g.printerMargin() + mcol * (g.imageWidth() + g.imageMargin()) - g.imageMargin() + 'mm')
+									.width(g.imageMargin() + 'mm')
+								setHorizontalLines($(this).find('line'), $(this).width())
+							}
+						}
+					})(col))
+				}
+				//new image box measure
+				addMeasure('#topDiv', (function(mcol){
+					return function(){
+						if(mcol >= g.imageMargin()){
+							$(this).remove()
+						}else{
+							$(this)
+								.css('left', g.printerMargin() + mcol * (g.imageWidth() + g.imageMargin()) + 'mm')
+								.width(g.imageWidth() + 'mm')
+							setHorizontalLines($(this).find('line'), $(this).width())
+						}
+					}
+				})(col))
+			}
 			_columns = v
 			this.calcImageWidth()
-			// TODO add boxes/measures
 			$('.box, .measure').trigger('mlayout')
 		},
 		rows: function(){
 			return _rows
 		},
 		setRows: function(v){
+			//not run if columns is not increased
+			for (let row = _rows; row < v; row++) {
+				for (let col = 0; col < _columns; col++) {
+					$('<div class="box"><img></div>')
+						.appendTo($('#printArea'))
+						.on('mlayout', boxLayoutFunction(col, row))
+						.on('dragenter', boxDragEnter)
+						.on('dragover', stopProp)
+						.on('dragleave', boxDragLeave)
+						.on('drop', boxDropFunction(col, row))
+						.find('img')
+							.attr('src', blobs[[col, row]] || placeholder)
+							.on('load', scaleImage)
+				}
+
+				if(row > 0){
+					addMeasure('#leftDiv', (function(mrow){
+						return function(){
+							if(mrow >= g.rows()){
+								$(this).remove()
+							}else{
+								$(this)
+									.css('top', g.printerMargin() + mrow * (g.imageHeight() + g.imageMargin()) - g.imageMargin() + 'mm')
+									.height(g.imageMargin() + 'mm')
+								setVerticalLines($(this).find('line'), $(this).height())
+							}
+						}
+					})(row))
+				}
+				addMeasure('#leftDiv', (function(mrow){
+					return function(){
+						if(mrow >= g.rows()){
+							$(this).remove()
+						}else{
+							$(this)
+								.css('top', g.printerMargin() + mrow * (g.imageHeight() + g.imageMargin()) + 'mm')
+								.height(g.imageHeight() + 'mm')
+							setVerticalLines($(this).find('line'), $(this).height())
+						}
+					}
+				})(row))
+			}
 			_rows = v
 			this.calcImageHeight()
 			// TODO add boxes/measures
@@ -142,41 +326,6 @@ const g = (function(){
 		},
 	}
 }())
-
-const blobs = {}
-
-function setHorizontalLines(lines, mwidth) {
-	lines[0].setAttribute('x1', 5)
-	lines[0].setAttribute('x2', mwidth - 5)
-	lines[0].setAttribute('y1', '50%')
-	lines[0].setAttribute('y2', '50%')
-
-	lines[1].setAttribute('x1', 5)
-	lines[1].setAttribute('x2', 5)
-	lines[1].setAttribute('y1', '0%')
-	lines[1].setAttribute('y2', '100%')
-
-	lines[2].setAttribute('x1', mwidth - 5)
-	lines[2].setAttribute('x2', mwidth - 5)
-	lines[2].setAttribute('y1', '0%')
-	lines[2].setAttribute('y2', '100%')
-}
-function setVerticalLines(lines, mheight) {
-	lines[0].setAttribute('y1', 5)
-	lines[0].setAttribute('y2', mheight - 5)
-	lines[0].setAttribute('x1', '50%')
-	lines[0].setAttribute('x2', '50%')
-
-	lines[1].setAttribute('y1', 5)
-	lines[1].setAttribute('y2', 5)
-	lines[1].setAttribute('x1', '0%')
-	lines[1].setAttribute('x2', '100%')
-
-	lines[2].setAttribute('y1', mheight - 5)
-	lines[2].setAttribute('y2', mheight - 5)
-	lines[2].setAttribute('x1', '0%')
-	lines[2].setAttribute('x2', '100%')
-}
 
 addMeasure('#topDiv', function(){
 	$(this)
@@ -228,64 +377,6 @@ $('#printArea').on('mlayout', function(e){stopProp(e)
 	$('.box, .measure').trigger('mlayout')
 })
 
-function boxLayoutFunction(mcol, mrow){
-	return function(e){stopProp(e)
-		if(mcol >= g.imageMargin() || mrow >= g.rows()){
-			$(this).remove()
-		}else{
-			$(this)
-				.width(g.imageWidth() + 'mm')
-				.height(g.imageHeight() + 'mm')
-				.css('top', mrow * (g.imageMargin() + g.imageHeight()) + 'mm')
-				.css('left', mcol * (g.imageMargin() + g.imageWidth()) + 'mm')
-				.find('img').trigger('load')
-		}
-	}
-}
-
-function boxDragEnter(e) {stopProp(e)
-	$(this)
-		.css('box-shadow', '0 0 0 20px green')
-		.css('z-index', '1')
-}
-
-function boxDragLeave(e) {stopProp(e)
-	$(this)
-		.css('box-shadow', 'initial')
-		.css('z-index', 'initial')
-}
-
-function boxDropFunction(mcol, mrow) {
-	//saving x as lx, otherwise x == g.imageMargin() onDrop for each box
-	return function(e) {stopProp(e)
-		if (e.originalEvent.dataTransfer.files.length > 0) {
-			let mURL = window.URL.createObjectURL(e.originalEvent.dataTransfer.files[0])
-			blobs[[mcol, mrow]] = mURL
-			$(this).find('img')
-				.attr('src', mURL)
-				.trigger('load')
-		}
-		$(this).trigger('dragleave')
-	}
-}
-
-function scaleImage() {
-	let widthScale = $(this).parent().width() / $(this).prop('naturalWidth')
-	let heightScale = $(this).parent().height() / $(this).prop('naturalHeight')
-
-	if (widthScale > heightScale) {
-		$(this)
-		.width(widthScale * $(this).prop('naturalWidth'))
-		.height(widthScale * $(this).prop('naturalHeight'))
-		.css('top', -0.5 * ($(this).height() - $(this).parent().height()))
-	} else {
-		$(this)
-		.width(heightScale * $(this).prop('naturalWidth'))
-		.height(heightScale * $(this).prop('naturalHeight'))
-		.css('left', -0.5 * ($(this).width() - $(this).parent().width()))
-	}
-}
-
 for (let col = 0; col < g.columns(); col++) {
 	for (let row = 0; row < g.rows(); row++) {
 		$('<div class="box"><img></div>')
@@ -305,7 +396,7 @@ for (let col = 0; col < g.columns(); col++) {
 	if(col > 0){
 		addMeasure('#topDiv', (function(mcol){
 			return function(){
-				if(mcol >= g.imageMargin()){
+				if(mcol >= g.columns()){
 					$(this).remove()
 				}else{
 					$(this)
@@ -318,7 +409,7 @@ for (let col = 0; col < g.columns(); col++) {
 	}
 	addMeasure('#topDiv', (function(mcol){
 		return function(){
-			if(mcol >= g.imageMargin()){
+			if(mcol >= g.columns()){
 				$(this).remove()
 			}else{
 				$(this)
@@ -360,6 +451,7 @@ for (let row = 0; row < g.rows(); row++) {
 }
 
 $('#mbutton').click(function(){
+	g.setColumns(g.columns()+1)
 	g.setRows(g.rows()+1)
 })
 
