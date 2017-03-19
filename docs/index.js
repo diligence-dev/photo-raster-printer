681078ee730bcce2beb719ce688911da35278ff9
Pascal Führlich <pascal.fuehrlich@student.hpi.de>
2017-03-19 20:39:40 +0100

    works

--- docs/index.js
+++ docs/index.js
@@ -2,8 +2,6 @@ const $ = require('jquery')
 const g = require('./mvarstore.js')
 const managerBoxesMeasures = require('./managerBoxesMeasures.js')
 
-const placeholder = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAABHNCSVQICAgIfAhkiAAAAA1JREFUCJlj+D/T+D8ABzECy352aNAAAAAASUVORK5CYII='
-
 const sideDivWidth = 80 //px
 const sideDivHeight = 50 //px
 const sideDivMargin = 10 //px
@@ -16,106 +14,20 @@ const leftSide = $('#leftSide')
 const rightSide = $('#rightSide')
 const bottomSide = $('#bottomSide')
 
-managerBoxesMeasures({topSide, leftSide, /*rightSide, bottomSide,*/ boxContainer: printArea, g})
-
 topSide.height(sideDivHeight).width('100%').css('top', -sideDivHeight - sideDivMargin)
 leftSide.width(sideDivWidth).height('100%').css('left', -sideDivWidth - sideDivMargin)
 
 rightSide.width(sideDivWidth).height('100%').css('right', -sideDivWidth - sideDivMargin)
 bottomSide.height(sideDivHeight).width('100%').css('bottom', -sideDivHeight - sideDivMargin)
 
-g.on_paperWidth_changed( () => { paper.trigger('mlayout') })
-g.on_paperHeight_changed( () => { paper.trigger('mlayout') })
+g.on_paperWidth_changed( () => {
+	paper.trigger('mlayout')
+	printArea.trigger('mlayout')})
+g.on_paperHeight_changed( () => {
+	paper.trigger('mlayout')
+	printArea.trigger('mlayout')})
 
 g.on_printerMargin_changed( () => { printArea.trigger('mlayout') })
-
-function addMeasure(parent, inputClass, inputValue, layoutFunc, inputFunc) {
-	const inp = $('<input class="' + inputClass + '" type="number">')
-		.change(function(){
-			$.proxy(inputFunc, $(this))()
-		})
-
-	$('<div class="measure screenAbsolute"><svg><g stroke-width="2px" stroke="black"><line/><line/><line/></g></svg></div>')
-		.append(inp)
-		.on('mlayout', function(e){
-			e.stopPropagation()
-			e.preventDefault()
-			//execute bound to this
-			$.proxy(layoutFunc, $(this))()
-		})
-		.appendTo(parent)
-}
-
-function setHorizontalLines(lines, mwidth) {
-	lines[0].setAttribute('x1', 5)
-	lines[0].setAttribute('x2', mwidth - 5)
-	lines[0].setAttribute('y1', '60%')
-	lines[0].setAttribute('y2', '60%')
-
-	lines[1].setAttribute('x1', 5)
-	lines[1].setAttribute('x2', 5)
-	lines[1].setAttribute('y1', '20%')
-	lines[1].setAttribute('y2', '100%')
-
-	lines[2].setAttribute('x1', mwidth - 5)
-	lines[2].setAttribute('x2', mwidth - 5)
-	lines[2].setAttribute('y1', '20%')
-	lines[2].setAttribute('y2', '100%')
-}
-function setVerticalLines(lines, mheight) {
-	lines[0].setAttribute('y1', 5)
-	lines[0].setAttribute('y2', mheight - 5)
-	lines[0].setAttribute('x1', '60%')
-	lines[0].setAttribute('x2', '60%')
-
-	lines[1].setAttribute('y1', 5)
-	lines[1].setAttribute('y2', 5)
-	lines[1].setAttribute('x1', '20%')
-	lines[1].setAttribute('x2', '100%')
-
-	lines[2].setAttribute('y1', mheight - 5)
-	lines[2].setAttribute('y2', mheight - 5)
-	lines[2].setAttribute('x1', '20%')
-	lines[2].setAttribute('x2', '100%')
-}
-
-addMeasure(topSide, 'printerMarginInp', g.printerMargin(), function(){
-	$(this)
-		.css('left', 0)
-		.width(g.printerMargin() + 'mm')
-	setHorizontalLines($(this).find('line'), $(this).width())
-}, function(){
-	g.printerMargin(Number($(this).val()))
-	$('.printerMarginInp').val(g.printerMargin())
-})
-addMeasure(topSide, 'printerMarginInp', g.printerMargin(), function(){
-	$(this)
-		.css('right', 0)
-		.width(g.printerMargin() + 'mm')
-	setHorizontalLines($(this).find('line'), $(this).width())
-}, function(){
-	g.printerMargin(Number($(this).val()))
-	$('.printerMarginInp').val(g.printerMargin())
-})
-
-addMeasure(leftSide, 'printerMarginInp', g.printerMargin(), function(){
-	$(this)
-		.css('top', 0)
-		.height(g.printerMargin() + 'mm')
-	setVerticalLines($(this).find('line'), $(this).height())
-}, function(){
-	g.printerMargin(Number($(this).val()))
-	$('.printerMarginInp').val(g.printerMargin())
-})
-addMeasure(leftSide, 'printerMarginInp', g.printerMargin(), function(){
-	$(this)
-		.css('bottom', 0)
-		.height(g.printerMargin() + 'mm')
-	setVerticalLines($(this).find('line'), $(this).height())
-}, function(){
-	g.printerMargin(Number($(this).val()))
-	$('.printerMarginInp').val(g.printerMargin())
-})
 
 $(window).on('resize', function(){
 	//center paper in body
@@ -146,18 +58,20 @@ printArea.on('mlayout', function(e){
 //g.on_printerMargin_changed(() => {console.log(g.printerMargin())})
 
 $('#mbutton').click(function(){
-	g.rows(g.rows() + 1)
-	g.columns(g.columns() + 1)
+	g.rows(g.rows() - 1)
+	g.columns(g.columns() - 1)
 })
 
+managerBoxesMeasures({topSide, leftSide, /*rightSide, bottomSide,*/ boxContainer: printArea, g})
+
 $(document).ready(function(){
-	g.columns(3)
 	g.rows(4)
-	// $('.printerMarginInp').first().val(g.printerMargin()).trigger('change')
+	g.columns(3)
+	$('.printerMarginInp').first().val(g.printerMargin()).trigger('change')
 	// $('.imageMarginInp').first().val(g.imageMargin()).trigger('change')
 	// $('.imageWidthInp').first().val(g.imageWidth()).trigger('change')
 	// $('.imageWidthInp').first().val(g.imageWidth()).trigger('change')
 	// $('.imageHeightInp').first().val(g.imageHeight()).trigger('change')
-	paper.trigger('mlayout')
+	g.paperWidth(g.paperWidth())
 	$(window).trigger('resize')
 })
--- docs/managerBoxesMeasures.js
+++ docs/managerBoxesMeasures.js
@@ -3,8 +3,7 @@ module.exports = function({topSide, leftSide, /*rightSide, bottomSide,*/ boxCont
 
 	const $ = require('jquery')
 	const boxesAndMeasures = []
-	function layoutBams(){
-		//bom = box or measure
+	function layoutBoxesAndMeasures(){
 		boxesAndMeasures.forEach( boxOrMeasure => { boxOrMeasure.trigger('mlayout') } )
 	}
 	//urls to blobs the user inserted
@@ -12,76 +11,76 @@ module.exports = function({topSide, leftSide, /*rightSide, bottomSide,*/ boxCont
 
 	const boxProto = {
 		init: function(col, row){
-			this.img = $(`<img src="${blobs[[col, row]] || placeholder}">`)
-				.on('load', this.scaleImage)
+			this.col = col
+			this.row = row
 			this.div = $('<div class="box"></div>')
-				.on('dragenter', this.boxDragEnter)
-				.on('dragover', this.stopProp)
-				.on('dragleave', this.boxDragLeave)
-				.on('mlayout', this.boxLayoutFunc(col, row))
-				.on('drop', this.boxDropFunc(col, row))
-				.append(this.img)
+				.on('dragenter', this.dragEnter.bind(this))
+				.on('dragover', e => {e.preventDefault();e.stopPropagation()})
+				.on('dragleave', this.dragLeave.bind(this))
+				.on('mlayout', this.layout.bind(this))
+				.on('drop', this.drop.bind(this))
 				.appendTo(boxContainer)
+			const url = blobs[[col, row]] || placeholder
+			this.img = $(`<img src="${url}">`)
+			this.img
+				.on('load', this.scaleImage.bind(this))
+				.appendTo(this.div)
 			boxesAndMeasures.push(this.div)
 		},
-		boxDragEnter: function(e) {
+		dragEnter: function(e) {
 			e.stopPropagation()
 			e.preventDefault()
 			this.div
 				.css('box-shadow', '0 0 0 20px green')
 				.css('z-index', '1')
 		},
-		boxDragLeave: function(e) {
+		dragLeave: function(e) {
 			e.stopPropagation()
 			e.preventDefault()
 			this.div
 				.css('box-shadow', 'initial')
 				.css('z-index', 'initial')
 		},
-		boxDropFunc: function(col, row) {
-			return function(e) {
-				e.stopPropagation()
-				e.preventDefault()
-				if (e.originalEvent.dataTransfer.files.length > 0) {
-					const mURL = window.URL.createObjectURL(e.originalEvent.dataTransfer.files[0])
-					blobs[[col, row]] = mURL
-					this.img
-						.attr('src', mURL)
-						.trigger('load')
-				}
-				this.div.trigger('dragleave')
+		drop: function(e) {
+			e.stopPropagation()
+			e.preventDefault()
+			if (e.originalEvent.dataTransfer.files.length > 0) {
+				const mURL = window.URL.createObjectURL(e.originalEvent.dataTransfer.files[0])
+				blobs[[this.col, this.row]] = mURL
+				this.img
+					.attr('src', mURL)
+					.trigger('load')
 			}
+			this.div.trigger('dragleave')
 		},
 		scaleImage: function() {
-			const widthScale = this.div.parent().width() / this.div.prop('naturalWidth')
-			const heightScale = this.div.parent().height() / this.div.prop('naturalHeight')
+			const widthScale = this.div.width() / this.img.prop('naturalWidth')
+			const heightScale = this.div.height() / this.img.prop('naturalHeight')
 
 			if (widthScale > heightScale) {
-				this.div
-					.width(widthScale * this.div.prop('naturalWidth'))
-					.height(widthScale * this.div.prop('naturalHeight'))
-					.css('top', -0.5 * (this.div.height() - this.div.parent().height()))
+				this.img
+					.width(widthScale * this.img.prop('naturalWidth'))
+					.height(widthScale * this.img.prop('naturalHeight'))
+					.css('top', -0.5 * (this.img.height() - this.div.height()))
 			} else {
-				this.div
-					.width(heightScale * this.div.prop('naturalWidth'))
-					.height(heightScale * this.div.prop('naturalHeight'))
-					.css('left', -0.5 * (this.div.width() - this.div.parent().width()))
+				this.img
+					.width(heightScale * this.img.prop('naturalWidth'))
+					.height(heightScale * this.img.prop('naturalHeight'))
+					.css('left', -0.5 * (this.img.width() - this.div.width()))
 			}
 		},
-		boxLayoutFunc: function(col, row){
-			return function(e){
-				e.stopPropagation()
-				e.preventDefault()
-				if(col >= g.columns() || row >= g.rows()){
-					this.div.remove()
-				}else{
-					this.div
-						.width(g.imageWidth() + 'mm')
-						.height(g.imageHeight() + 'mm')
-						.css('top', row * (g.imageMargin() + g.imageHeight()) + 'mm')
-						.css('left', col * (g.imageMargin() + g.imageWidth()) + 'mm')
-					this.img.trigger('load')
-				}
+		layout: function(e){
+			e.stopPropagation()
+			e.preventDefault()
+			if(this.col >= g.columns() || this.row >= g.rows()){
+				this.div.remove()
+			}else{
+				this.div
+					.width(g.imageWidth() + 'mm')
+					.height(g.imageHeight() + 'mm')
+					.css('top', this.row * (g.imageMargin() + g.imageHeight()) + 'mm')
+					.css('left', this.col * (g.imageMargin() + g.imageWidth()) + 'mm')
+				this.img.trigger('load')
 			}
 		}
 	}
@@ -100,14 +99,14 @@ module.exports = function({topSide, leftSide, /*rightSide, bottomSide,*/ boxCont
 
 			const input = $('<input class="' + this.inputClass + '" type="number">')
 			input
-				.change( () => { this.onInputChanged(input.val()) })
+				.change( () => { this.onInputChanged(Number( input.val() )) })
 				.appendTo(this.div)
 
 			this.lines = this.div.find('line')
 			this.setupLines(this.lines)
 
 			this.div
-				.on('mlayout', this.layout)
+				.on('mlayout', this.layout.bind(this))
 				.appendTo(topSide)
 			boxesAndMeasures.push(this.div)
 		},
@@ -140,17 +139,36 @@ module.exports = function({topSide, leftSide, /*rightSide, bottomSide,*/ boxCont
 			}
 		}
 	}
+
+	//add printer margin measures
+	Object.create(horizontalMeasureProto).configure({
+		inputClass: 'printerMarginInp',
+		onInputChanged: inputValue => {
+			g.printerMargin(inputValue)
+			$('.printerMarginInp').val(inputValue) },
+		getLeft: () => 0,
+		getWidth: () => g.printerMargin()
+	}).init(-1)
+	Object.create(horizontalMeasureProto).configure({
+		inputClass: 'printerMarginInp',
+		onInputChanged: inputValue => {
+			g.printerMargin(inputValue)
+			$('.printerMarginInp').val(inputValue) },
+		getLeft: () => g.paperWidth() - g.printerMargin(),
+		getWidth: () => g.printerMargin()
+	}).init(-1)
+
 	const horizontalImageMeasureProto = Object.create(horizontalMeasureProto).configure({
 		inputClass: 'imageWidthInp',
 		onInputChanged: inputValue => {
 			console.log('todo: imageWidthInp changes imageWidth')
-			g.imageMargin( Number(inputValue) ) },
+			g.imageMargin(inputValue) },
 		getLeft: mcol => g.printerMargin() + mcol * (g.imageWidth() + g.imageMargin()),
 		getWidth: () => g.imageWidth()
 	})
 	const horizontalMarginMeasureProto = Object.create(horizontalMeasureProto).configure({
 		inputClass: 'imageMarginInp',
-		onInputChanged: inputValue => {	g.imageMargin( Number(inputValue) ) },
+		onInputChanged: inputValue => {	g.imageMargin(inputValue) },
 		getLeft: mcol => g.printerMargin() + mcol * (g.imageWidth() + g.imageMargin()) - g.imageMargin(),
 		getWidth: () => g.imageMargin()
 	})
@@ -167,7 +185,7 @@ module.exports = function({topSide, leftSide, /*rightSide, bottomSide,*/ boxCont
 			}
 		}
 	})
-	g.on_columns_changed(layoutBams)
+	g.on_columns_changed(layoutBoxesAndMeasures)
 
 	const verticalMeasureProto = {
 		configure: function({inputClass, onInputChanged, getTop, getHeight}){
@@ -183,14 +201,14 @@ module.exports = function({topSide, leftSide, /*rightSide, bottomSide,*/ boxCont
 
 			const input = $('<input class="' + this.inputClass + '" type="number">')
 			input
-				.change( () => { this.onInputChanged(input.val()) })
+				.change( () => { this.onInputChanged(Number( input.val() )) })
 				.appendTo(this.div)
 
 			this.lines = this.div.find('line')
 			this.setupLines(this.lines)
 
 			this.div
-				.on('mlayout', this.layout)
+				.on('mlayout', this.layout.bind(this))
 				.appendTo(leftSide)
 			boxesAndMeasures.push(this.div)
 		},
@@ -207,7 +225,7 @@ module.exports = function({topSide, leftSide, /*rightSide, bottomSide,*/ boxCont
 			lines[2].setAttribute('x1', '20%')
 			lines[2].setAttribute('x2', '100%')
 		},
-		layoutFunc: function(e){
+		layout: function(e){
 			e.stopPropagation()
 			e.preventDefault()
 			if(this.row >= g.rows()){
@@ -223,19 +241,38 @@ module.exports = function({topSide, leftSide, /*rightSide, bottomSide,*/ boxCont
 			}
 		}
 	}
+
+	//add printer margin measures
+	Object.create(verticalMeasureProto).configure({
+		inputClass: 'printerMarginInp',
+		onInputChanged: inputValue => {
+			g.printerMargin(inputValue)
+			$('.printerMarginInp').val(inputValue) },
+		getTop: () => 0,
+		getHeight: () => g.printerMargin()
+	}).init(-1)
+	Object.create(verticalMeasureProto).configure({
+		inputClass: 'printerMarginInp',
+		onInputChanged: inputValue => {
+			g.printerMargin(inputValue)
+			$('.printerMarginInp').val(inputValue) },
+		getTop: () => g.paperHeight() - g.printerMargin(),
+		getHeight: () => g.printerMargin()
+	}).init(-1)
+
 	const verticalImageMeasureProto = Object.create(verticalMeasureProto).configure({
 		inputClass: 'imageHeightInp',
 		onInputChanged: inputValue => {
 			console.log('todo: imageHeightInp changes imageHeight')
-			g.imageMargin( Number(inputValue) ) },
-		getTopFunction: mrow => g.printerMargin() + mrow * (g.imageWidth() + g.imageMargin()),
-		getHeightFunction: () => g.imageHeight()
+			g.imageMargin(inputValue) },
+		getTop: mrow => g.printerMargin() + mrow * (g.imageHeight() + g.imageMargin()),
+		getHeight: () => g.imageHeight()
 	})
 	const verticalMarginMeasureProto = Object.create(verticalMeasureProto).configure({
 		inputClass: 'imageMarginInp',
-		onInputChanged: inputValue => { g.imageMargin( Number(inputValue) ) },
-		getLeftFunction: mrow => g.printerMargin() + mrow * (g.imageWidth() + g.imageMargin()) - g.imageMargin(),
-		getWidthFunction: () => g.imageMargin()
+		onInputChanged: inputValue => { g.imageMargin(inputValue) },
+		getTop: mrow => g.printerMargin() + mrow * (g.imageHeight() + g.imageMargin()) - g.imageMargin(),
+		getHeight: () => g.imageMargin()
 	})
 
 	g.preRowsChange(function(prevRows, currRows){
@@ -250,12 +287,12 @@ module.exports = function({topSide, leftSide, /*rightSide, bottomSide,*/ boxCont
 			}
 		}
 	})
-	g.on_rows_changed(layoutBams)
+	g.on_rows_changed(layoutBoxesAndMeasures)
 
 
-	g.on_paperWidth_changed(layoutBams)
-	g.on_paperHeight_changed(layoutBams)
+	g.on_paperWidth_changed(layoutBoxesAndMeasures)
+	g.on_paperHeight_changed(layoutBoxesAndMeasures)
 
-	g.on_printerMargin_changed(layoutBams)
-	g.on_imageMargin_changed(layoutBams)
+	g.on_printerMargin_changed(layoutBoxesAndMeasures)
+	g.on_imageMargin_changed(layoutBoxesAndMeasures)
 }
