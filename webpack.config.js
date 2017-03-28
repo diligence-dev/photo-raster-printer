377bff61ecd97e7d65ca60ab6e2a52657a282a46
Pascal Führlich <pascal.fuehrlich@web.de>
2017-03-28 21:57:00 +0200

    webpack babel uglify

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports =  {
+	entry: './js/main.js',
+	output: {
+		filename: './docs/bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['env']
+					}
+				}
+			}
+		]
+	}
+}
